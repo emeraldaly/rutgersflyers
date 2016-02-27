ff31528319623f4751cc264a6216240ea0e33afe
eliezerwohl <eliezerwohl@gmail.com>
2016-02-27 11:01:01 -0500

    hk

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
-var mysql = require('mysql');
-console.log(process.env.JAWSDB_URL);
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-console.log("connection to db");
-console.log(connection);
-connection.connect();
-module.exports = connection;
+// var mysql = require('mysql');
+// console.log(process.env.JAWSDB_URL);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// console.log("connection to db");
+// console.log(connection);
+// connection.connect();
+// module.exports = connection;
 
-connection.connect();
+// connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
  
-  console.log('The solution is: ', rows[0].solution);
-});
+//   console.log('The solution is: ', rows[0].solution);
+// });
  
-connection.end();
+// connection.end();
