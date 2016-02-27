ad8c7c56354f1bb06f1ef6232d8f5142dbb740b5
evan bates <evan.bates@rutgers.edu>
2016-02-27 16:18:56 -0500

    cleardb

--- models/models.js
+++ models/models.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var Sequelize = require('sequelize');
 if(process.env.NODE_ENV === 'production') {
   // HEROKU DB
-  console.log(process.env.JAWSDB_URL);
-  var connection = new Sequelize(process.env.JAWSDB_URL);
+  console.log(process.env.CLEARDB_DATABASE_URL);
+  var connection = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } 
 else {
   // LOCAL DB
