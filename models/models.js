a235e43b804cb72278303dacb0e9d1d37bfdc552
evan bates <evan.bates@rutgers.edu>
2016-02-26 20:37:24 -0500

    hk

--- models/models.js
+++ models/models.js
@@ -1,7 +1,7 @@
 //database setup
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
-var connection = new Sequelize(JAWSDB_URL);
+var connection = new Sequelize(process.env.JAWSDB_URL);
 
 
 var User = connection.define ('User',{
