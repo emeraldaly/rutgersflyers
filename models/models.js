f4f86e4519bf940ee8981502ccfa79a752d849bb
Alyson <emerald.aly@gmail.com>
2016-02-27 20:06:45 -0500

    added js with tab folder

--- models/models.js
+++ models/models.js
@@ -73,7 +73,17 @@ connection.sync();
 
 // Venue.bulkCreate([
 // { name: 'The Frog and the Peach', address: '29 Dennis St', phoneNumber: '(732)846-3216', website: 'frogandpeach.com' },
-//  { name: 'RU Hungry', address: 'New Brunswick', phoneNumber: '(732)246-2177', website: 'http://ruhungrynj.net/' }
+// { name: 'RU Hungry', address: 'New Brunswick', phoneNumber: '(732)246-2177', website: 'http://ruhungrynj.net/' }
+// { name: 'NJ Transit', address: 'French & Albany Sts., New Brunswick, NJ 08901', phoneNumber: '(973) 275-5555', website: 'http://www.njtransit.com/' }      
+// { name: 'NJ Bus Station', address: '750 Somerset St., New Brunswick, NJ 08901', phoneNumber: '(732) 249-1100', website: 'http://www.coachusa.com/' }    
+// { name: 'Newark Airport', address: '3 Brewster Rd, Newark, NJ 07114', phoneNumber: '(973) 961-6000', website: 'http://www.panynj.gov/airports/newark-liberty.html' }    
+// { name: 'JFK Airport', address: 'New York, NY 11430', phoneNumber: '(718) 244-4444', website: 'http://www.panynj.gov/airports/jfk.html' }    
+// { name: 'LaGuardia Airport', address: 'New York, NY 11371', phoneNumber: '(718) 533-3400', website: 'http://www.panynj.gov/airports/laguardia.html' }    
+// { name: 'RU NextBus', address: '', phoneNumber: '', website: '' }    
+// { name: 'Uber', address: '', phoneNumber: '', website: '' }    
+// { name: 'Yellow Cab', address: '', phoneNumber: '', website: '' } 
+// { name: 'SEPTA', address: '1234 Market Street, Philadelphia, PA 19107', phoneNumber: '(215) 580-7800', website: 'http://www.septa.org/' }  
+// { name: 'PATH', address: '', phoneNumber: '', website: 'http://www.panynj.gov/path/' }         
 // ]);
 
 // Category.bulkCreate([
