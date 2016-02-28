96bfc951763365a0f789cc4303a779670e442818
evan bates <evan.bates@rutgers.edu>
2016-02-27 20:19:20 -0500

    hk

--- server1.js
+++ server1.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 9000;
-
+var dotenv = require('dotenv');
 var expressHandlebars = require('express-handlebars');
 //passport
 var passport = require('passport');
