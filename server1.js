96a7d47a0c4479d6c78bd1af61dbddc07e10043b
evan bates <evan.bates@rutgers.edu>
2016-02-27 20:03:06 -0500

    hk

--- server1.js
+++ server1.js
@@ -1,3 +1,4 @@
+//express setup
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 9000;
@@ -197,52 +198,6 @@ app.get('/logout', function (req, res){
     res.redirect('/'); //Inside a callback… bulletproof!
   });
 });
-
-//database connection
-  app.listen(PORT, function() {
-      console.log("Listening on:" + PORT)
-  });/express setup
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 9000;
-
-var expressHandlebars = require('express-handlebars');
-//passport
-var passport = require('passport');
-var passportLocal = require('passport-local');
-var session = require('express-session');
-
-//bcrypt
-var bcrypt = require("bcryptjs");
-
-//bodyParser
-var bodyParser = require('body-parser');
-require('dotenv').config();
-//middleware
-app.use(express.static('public'));
-app.use(require('express-session')({
-  secret: "rutgerpridesecrets",
-  resave: true,
-  saveUninitialized: true,
-  cookie: {secure: false, maxAge: (1000 * 60 * 60 * 24 * 30) },
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-passport.deserializeUser(function(id, done) {
-    done(null, { id: id, username: id })
-});
-app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-var routes = require('./controllers/rutgersController.js');
-app.use('/', routes);
 
 //database connection
   app.listen(PORT, function() {
