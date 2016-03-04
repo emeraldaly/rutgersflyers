7d97dd69b7134e2c16dc9878518201c42466f881
eliezerwohl <eliezerwohl@gmail.com>
2016-03-04 14:48:53 -0500

    merge

--- server.js
+++ server.js
@@ -554,15 +554,7 @@ app.get('/food', function(req,res) {
   });
 
 
-<<<<<<< HEAD
-app.post('/review/:venueId', function(req, res) {
-  Review.create({
-    review: req.body.review,
-    rating:9,
-    VenueId: req.params.venueId
-  }).then(function() {
-    res.redirect('back');
-=======
+
   app.post('/review/:venueId', function(req, res) {
     Review.create({
       review: req.body.review,
@@ -571,7 +563,6 @@ app.post('/review/:venueId', function(req, res) {
     }).then(function() {
       res.redirect('back');
     });
->>>>>>> b9050d208634632400e0c3a1b0ffbf8b87ee3771
   });
 
   connection.sync();
